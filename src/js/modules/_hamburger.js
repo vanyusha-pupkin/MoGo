862488e52d66b17f923db1ebbb79403078721025
vany pupkin <vany_pupkin@mail.ru>
2021-03-06 10:20:16 +0600

    Update 2021-03-06T04:19:09.252Z

--- src/js/modules/_hamburger.js
+++
@@ -1,8 +0,0 @@
-
-var hamburger = document.querySelector('.page-header__burger');
-var mainNav = document.querySelector('.main-nav__list');
-
-hamburger.addEventListener('click', function() {
-  hamburger.classList.toggle('active');
-  mainNav.classList.toggle('open')
-})
