23ae77cbfe15dde1da5e4dfb8aefabde78c30d34
vany pupkin <vany_pupkin@mail.ru>
2021-03-12 17:15:52 +0600

    adaptive index.html

--- src/js/modules/_hamburger.js
+++ src/js/modules/_hamburger.js
@@ -1,8 +1,10 @@
 
-// var hamburger = document.querySelector('.page-header__burger');
-// var mainNav = document.querySelector('.main-nav__list');
+var hamburger = document.querySelector('.page-header__burger');
+var mainNav = document.querySelector('.main-nav');
+var pageBody = document.querySelector('.page__body');
 
-// hamburger.addEventListener('click', function() {
-//   hamburger.classList.toggle('active');
-//   mainNav.classList.toggle('open')
-// })
+hamburger.addEventListener('click', function() {
+  hamburger.classList.toggle('active');
+  mainNav.classList.toggle('main-nav--open')
+  pageBody.classList.toggle('page__body--lock')
+})
